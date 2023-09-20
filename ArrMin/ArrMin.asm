24258c0815b24b6c427adb73b2e4a3bf7f63d5e1
unknown <danielmimportant@hotmail.com>
2023-09-20 17:42:59 +0930

    first version of ArrMin to be tested

--- ArrMin/ArrMin.asm
+++ ArrMin/ArrMin.asm
@@ -2,3 +2,56 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+    @R1
+    D=M
+    @END
+    D;JEQ
+    @last
+    M=D
+    @R2
+    D=M
+    @last
+    M=M+D
+    @R1
+    A=M
+    D=M
+    @R0
+    M=D
+    @R1
+    D=M
+    @index
+    M=D+1
+(LOOP)
+    @index
+    D=M
+    @last
+    D=M-D
+    @END
+    D;JEQ
+
+    @index
+    A=M
+    D=M
+    @R0
+    D=D-M
+    @NEW
+    D;JLT
+    @INC
+    0;JMP
+
+(NEW)
+    @index
+    A=M
+    D=M
+    @R0
+    M=D
+(INC)
+    @index
+    M=M+1
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
