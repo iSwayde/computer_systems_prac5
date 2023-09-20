78f03e9399ed71f9373ac3d4062c7b014545b497
unknown <danielmimportant@hotmail.com>
2023-09-20 15:59:14 +0930

    added Mult.asm to gauge functionality requirements from gradescope

---
+++ Mult.asm
@@ -0,0 +1,36 @@
+// This file is based on part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: Mult.asm
+
+// Multiplies R1 and R2 and stores the result in R0.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+
+// Put your code here.
+    @sum
+    M=0
+    @R1
+    D=M
+    @count
+    M=D
+(LOOP)
+    @count
+    D=M
+    @SET
+    D;JLE
+    @R2
+    D=M
+    @sum
+    M=D+M
+    @count
+    M=M-1
+    @LOOP
+    0;JMP
+(SET)
+    @sum
+    D=M
+    @R0
+    M=D
+(END)
+    @END
+    0;JMP
